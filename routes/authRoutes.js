10669c5a67af8465bfba91932427273f62417801
Enrique Davis <114179772+edavisx@users.noreply.github.com>
2025-05-08 17:29:23 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,4 +5,9 @@ const authController = require('../controllers/authController');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
+// agregando lo del examen, tabla users2
+
+router.post('/register2', authController.registerk2);
+router.post('/login2', authController.logink2);
+
 module.exports = router;
